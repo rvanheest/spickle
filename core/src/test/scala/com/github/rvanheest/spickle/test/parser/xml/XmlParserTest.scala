31ef925dc8be4a08cfa806817c4a3d6a3dd87160
Richard van Heest <richard.van.heest@dans.knaw.nl>
2017-03-23 23:12:53 +0100

    attribute parser tests

--- core/src/test/scala/com/github/rvanheest/spickle/test/parser/xml/XmlParserTest.scala
+++ core/src/test/scala/com/github/rvanheest/spickle/test/parser/xml/XmlParserTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.{ FlatSpec, Inside, Matchers }
 import com.github.rvanheest.spickle.parser.xml.XmlParser._
 
 import scala.util.{ Failure, Success }
-import scala.xml.Utility
+import scala.xml.{ NamespaceBinding, TopScope, Utility }
 
 class XmlParserTest extends FlatSpec with Matchers with Inside {
 
@@ -151,6 +151,123 @@ class XmlParserTest extends FlatSpec with Matchers with Inside {
         // @formatter:off
         nodes shouldBe Seq(<baz>!</baz>, <foo><bar>hello</bar><bar>world</bar><baz>!</baz></foo>)
         // @formatter:on
+    }
+  }
+
+  "attribute" should "read the attribute with a given name from the first node in the input, transform it using the given constructor function and keep the attribute in the remaining node" in {
+    // @formatter:off
+    val input = <foo test="123" hello="abc">bar</foo>
+    // @formatter:on
+    inside(attribute("test")(_.toInt).run(input)) {
+      case (Success(n), remainder) =>
+        n shouldBe 123
+        remainder shouldBe input
+    }
+  }
+
+  it should "fail when the first node in the input does not contain an attribute with the given name" in {
+    // @formatter:off
+    val input = <foo test="123" hello="abc">bar</foo>
+    // @formatter:on
+    inside(attribute("tset")(_.toInt).run(input)) {
+      case (Failure(e), remainder) =>
+        e shouldBe a[NoSuchElementException]
+        e.getMessage shouldBe "empty parser"
+        remainder shouldBe input
+    }
+  }
+
+  it should "fail when the attribute has an empty value" in {
+    // @formatter:off
+    val input = <foo test="" hello="abc">bar</foo>
+    // @formatter:on
+    inside(attribute("test")(_.toInt).run(input)) {
+      case (Failure(e), remainder) =>
+        e shouldBe a[NoSuchElementException]
+        e.getMessage shouldBe "empty parser"
+        remainder shouldBe input
+    }
+  }
+
+  it should "fail when the input is empty" in {
+    inside(attribute("tset")(_.toInt).run(Seq.empty)) {
+      case (Failure(e), remainder) =>
+        e shouldBe a[NoSuchElementException]
+        e.getMessage shouldBe "you're trying to parse an attribute in an empty xml Node"
+        remainder shouldBe empty
+    }
+  }
+
+  it should "catch any exception that is thrown in the constructor function" in {
+    // @formatter:off
+    val input = <foo test="123" hello="abc">bar</foo>
+    // @formatter:on
+    val error = new Exception("--error--")
+    inside(attribute("test")(_ => throw error).run(input)) {
+      case (Failure(e), remainder) =>
+        e shouldBe error
+        remainder shouldBe input
+    }
+  }
+
+  "attributeId" should "return the exact text that was found in the attribute" in {
+    // @formatter:off
+    val input = <foo test="123" hello="abc">bar</foo>
+    // @formatter:on
+    inside(attributeId("test").run(input)) {
+      case (Success(n), remainder) =>
+        n shouldBe "123"
+        remainder shouldBe input
+    }
+  }
+
+  "namespaceAttribute" should "parse a namespaced attribute" in {
+    // @formatter:off
+    val input = <foo xlink:type="simple" hello="abc">bar</foo>
+    // @formatter:on
+    implicit val ns = NamespaceBinding("xlink", "http://www.w3.org/1999/xlink", TopScope)
+    inside(namespaceAttribute("type").run(input)) {
+      case (Success(s), remainder) =>
+        s shouldBe "simple"
+        remainder shouldBe input
+    }
+  }
+
+  it should "fail if the attribute does not have the proper namespace" in {
+    // @formatter:off
+    val input = <foo ylink:type="simple" hello="abc">bar</foo>
+    // @formatter:on
+    implicit val ns = NamespaceBinding("xlink", "http://www.w3.org/1999/xlink", TopScope)
+    inside(namespaceAttribute("type").run(input)) {
+      case (Failure(e), remainder) =>
+        e shouldBe a[NoSuchElementException]
+        e.getMessage shouldBe "empty parser"
+        remainder shouldBe input
+    }
+  }
+
+  it should "fail if the attribute isn't there at all" in {
+    // @formatter:off
+    val input = <foo hello="abc">bar</foo>
+    // @formatter:on
+    implicit val ns = NamespaceBinding("xlink", "http://www.w3.org/1999/xlink", TopScope)
+    inside(namespaceAttribute("type").run(input)) {
+      case (Failure(e), remainder) =>
+        e shouldBe a[NoSuchElementException]
+        e.getMessage shouldBe "empty parser"
+        remainder shouldBe input
+    }
+  }
+
+  it should "take the first when two the same attributes are in the node" in {
+    // @formatter:off
+    val input = <foo xlink:type="simple" xlink:href="#bar" hello="abc">bar</foo>
+    // @formatter:on
+    implicit val ns = NamespaceBinding("xlink", "http://www.w3.org/1999/xlink", TopScope)
+    inside(namespaceAttribute("type").run(input)) {
+      case (Success(s), remainder) =>
+        s shouldBe "simple"
+        remainder shouldBe input
     }
   }
 }
